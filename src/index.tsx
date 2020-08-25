02994d790c308ad44d47b02c964e7ab4d5d57850
Tyler Long <tyler4long@gmail.com>
2020-08-25 10:19:34 -0700

    Hello React

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,12 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return 'Hello world';
+  }
+}
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+ReactDOM.render(<App />, container);
