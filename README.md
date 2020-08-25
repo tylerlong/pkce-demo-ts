18c06f9f0cb2555b3e85dc5bc97bec3f3e1e88d6
Tyler Long <tyler4long@gmail.com>
2020-08-25 10:13:10 -0700

    Setup the project

---
+++ README.md
@@ -0,0 +1,3 @@
+# PKCE demo
+
+Ref: https://medium.com/ringcentral-developers/use-authorization-code-pkce-for-ringcentral-api-in-client-app-e9108f04b5f0
---
+++ webpack.config.ts
@@ -0,0 +1,39 @@
+/* eslint-disable node/no-unpublished-import */
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import path from 'path';
+import dotenv from 'dotenv-override-true';
+import {DefinePlugin} from 'webpack';
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'RingCentral PKCE demo',
+    }),
+    new DefinePlugin({
+      'process.env': JSON.stringify(dotenv.config().parsed),
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+};
+
+export default [config];
