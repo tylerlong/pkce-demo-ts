9a3c764a24b516553e2045fc406f2cf6e00cef80
Tyler Long <tyler4long@gmail.com>
2020-10-07 13:13:22 -0700

    Non-PKCE

--- src/store.ts
+++ src/store.ts
@@ -2,7 +2,7 @@ import SubX from 'subx';
 import {TokenInfo} from '@rc-ex/core/lib/definitions';
 import RingCentral from '@rc-ex/core';
 import AuthorizeUriExtension from '@rc-ex/authorize-uri';
-import localforage from 'localforage';
+// import localforage from 'localforage';
 import axios from 'axios';
 import qs from 'qs';
 
@@ -10,6 +10,7 @@ const redirectUri = window.location.origin + window.location.pathname;
 const rc = new RingCentral({
   server: process.env.RINGCENTRAL_SERVER_URL,
   clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
 });
 const urlSearchParams = new URLSearchParams(
   new URL(window.location.href).search
@@ -21,10 +22,10 @@ if (code === null) {
   rc.installExtension(authorizeUriExtension);
   authorizeUri = authorizeUriExtension.buildUri({
     redirect_uri: redirectUri,
-    code_challenge_method: 'S256',
+    // code_challenge_method: 'S256',
   });
-  const codeVerifier = authorizeUriExtension.codeVerifier;
-  localforage.setItem('code_verifier', codeVerifier);
+  // const codeVerifier = authorizeUriExtension.codeVerifier;
+  // localforage.setItem('code_verifier', codeVerifier);
 }
 
 export type StoreType = {
@@ -40,7 +41,7 @@ const store = SubX.proxy<StoreType>({
       this.token = await rc.authorize({
         code,
         redirect_uri: redirectUri,
-        code_verifier: (await localforage.getItem('code_verifier')) as string,
+        // code_verifier: (await localforage.getItem('code_verifier')) as string,
       });
       const r = await axios.post(
         process.env.RINGCENTRAL_SERVER_URL + '/restapi/oauth/token',
